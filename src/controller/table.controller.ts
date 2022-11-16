c4e3189141d846465adf0d139da4c391996b0941
Liluminalia <peloazul4@gmail.com>
2022-11-16 18:49:20 +0100

    adding controller and interface of tables

---
+++ src/controller/table.controller.ts
@@ -0,0 +1,92 @@
+import { NextFunction, Request, Response } from 'express';
+import { Data } from '../data/data.js';
+import { HTTPError } from '../interfaces/error.js';
+import { Table } from '../interfaces/tables.js';
+
+export class TableController {
+    constructor(public dataModel: Data<Table>) {}
+    async getAllController(req: Request, res: Response, next: NextFunction) {
+        try {
+            const data = await this.dataModel.getAll();
+            res.json(data).end();
+        } catch (error) {
+            const newError = new HTTPError(
+                503,
+                'service unavailable',
+                (error as Error).message
+            );
+            next(newError);
+            return;
+        }
+    }
+    async getController(req: Request, res: Response, next: NextFunction) {
+        try {
+            const data = await this.dataModel.get(+req.params.id);
+            res.json(data).end();
+        } catch (error) {
+            next(this.#createHttpError(error as Error));
+            return;
+        }
+    }
+
+    async postController(req: Request, res: Response, next: NextFunction) {
+        if (!req.body.name) {
+            const newError = new HTTPError(
+                406,
+                'not acceptable',
+                'nombre no incluido'
+            );
+            next(newError);
+            return;
+        }
+        try {
+            const newTable = await this.dataModel.post(req.body);
+            res.json(newTable).end();
+        } catch (error) {
+            const newError = new HTTPError(
+                503,
+                'service unavailable',
+                (error as Error).message
+            );
+            next(newError);
+            return;
+        }
+    }
+    async patchController(req: Request, res: Response, next: NextFunction) {
+        try {
+            const updateTable = await this.dataModel.patch(
+                +req.params.id,
+                req.body
+            );
+            res.json(updateTable).end();
+        } catch (error) {
+            next(this.#createHttpError(error as Error));
+            return;
+        }
+    }
+    async deleteController(req: Request, res: Response, next: NextFunction) {
+        try {
+            await this.dataModel.delete(+req.params.id);
+            res.json({}).end();
+        } catch (error) {
+            next(this.#createHttpError(error as Error));
+            return;
+        }
+    }
+    #createHttpError(error: Error) {
+        if ((error as Error).message === 'Not found id') {
+            const httpError = new HTTPError(
+                404,
+                'Not Found',
+                (error as Error).message
+            );
+            return httpError;
+        }
+        const httpError = new HTTPError(
+            503,
+            'Service unavailable',
+            (error as Error).message
+        );
+        return httpError;
+    }
+}
