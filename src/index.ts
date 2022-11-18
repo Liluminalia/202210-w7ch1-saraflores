17f15d9258794e05234e717e99884ae8128327f1
Liluminalia <peloazul4@gmail.com>
2022-11-18 11:43:51 +0100

    saving changes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import http from 'http';
 import { app } from './app.js';
+import debug1 from 'debug';
+const debug = debug1('http');
 import { CustomError } from './interfaces/error.js';
 import { dbConnect } from './router/db.conect.js';
 const port = process.env.PORT || 3300;
@@ -16,7 +18,7 @@ server.on('listening', () => {
                 ? `http://localhost:${addr?.port}`
                 : `port ${addr?.port}`;
     }
-    console.log(`Listening on ${bind}`);
+    debug(`Listening on ${bind}`);
 });
 
 server.on('error', (error: CustomError, response: http.ServerResponse) => {
